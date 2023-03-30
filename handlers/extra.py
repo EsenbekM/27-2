da4eea9616fd089a7a890f09be66a1193b05566c
EsenbekM <esenbekm03@gmail.com>
2023-03-30 21:56:16 +0600

    lesson 7

--- handlers/extra.py
+++ handlers/extra.py
@@ -1,30 +1,18 @@
 from aiogram import types, Dispatcher
 from config import bot, OPENAI_TOKEN
 import openai
+from .loader import download_audio
 
 openai.api_key = OPENAI_TOKEN
 
 
 # @dp.message_handler()
 async def filter_bad_words(message: types.Message):
-    bad_words = ['html', 'css', 'js', 'дурак', 'жинди']
-    username = f"@{message.from_user.username}" \
-        if message.from_user.username is not None else message.from_user.full_name
-    for word in bad_words:
-        if word in message.text.lower().replace(' ', ''):
-            await message.answer(f"Не матерись {username}, Сам ты {word}")
-            # await bot.delete_message(message.chat.id, message.message_id)
-            await message.delete()
-            break
+    if "youtube.com" in message.text:
+        await message.answer("Loading...")
+        video = open(download_audio(message.text), "rb")
+        await message.answer_audio(video)
 
-    if message.text.startswith('.'):
-        # await bot.pin_chat_message(message.chat.id, message.message_id)
-        await message.pin()
-
-    if message.text == 'dice':
-        a = await bot.send_dice(message.chat.id)
-        # print(a.dice.value)
-        # await message.answer_dice(emoji="🎳")
 
     response = openai.Completion.create(
         model="text-davinci-003",
